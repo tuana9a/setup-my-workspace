c7e58d5615ee11807d56bcec9c6ec30bc34c5a00
Tuan Nguyen Minh <tuana9a@gmail.com>
2023-02-16 11:40:07 +0700

    init

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt install htop -y
+sudo apt install net-tools -y
+sudo apt install nginx -y
+sudo apt install openssh-server -y
+sudo apt isntall openvpn -y
+sudo apt install python3.8 python3.8-dev python3.8-distutils python3.8-venv -y
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - && sudo apt-get install -y nodejs
