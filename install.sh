bcb8813eab1f72813dc824d5179ade184fd25fa2
Tuan Nguyen Minh <tuana9a@gmail.com>
2023-02-27 09:08:46 +0700

    add install docker and docker-compose

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@ sudo apt install htop -y
 sudo apt install net-tools -y
 sudo apt install nginx -y
 sudo apt install openssh-server -y
-sudo apt isntall openvpn -y
+sudo apt install openvpn -y
 sudo apt install python3.8 python3.8-dev python3.8-distutils python3.8-venv -y
+
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - && sudo apt-get install -y nodejs
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
